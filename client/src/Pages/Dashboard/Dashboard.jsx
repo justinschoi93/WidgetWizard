b4a302275cc9dc1cb9c738aa7c13b8a9acd07753
jOwl <jhome@js-MacBook-Pro-2.local>
2024-05-02 12:09:43 -0700

    About, Dashboard

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,19 @@
 import './dashboard.css';
 // import Widgets from '../../Components/Widgets/Widgets';
 import Clock from '../../Components/Widgets/Clock/Clock';
-
+import Pokedex from '../../Components/Widgets/Pokedex/Pokedex';
 const Dashboard = () => {
     return (
         <section className="dashboard__container">
-            {/* {Widgets.map((Widget, index) => {
-                return <Widget key={index} />
-            })} */}
+            <Pokedex />
             <Clock />
         </section>
     )
 }
 
 export default Dashboard;
+
+
+{/* {Widgets.map((Widget, index) => {
+                return <Widget key={index} />
+            })} */}
