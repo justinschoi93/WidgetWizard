e44d91866091fc4aae5da93c8f9cdd9c4b5a15d8
jOwl <jhome@js-MacBook-Pro-2.local>
2024-05-03 13:49:34 -0700

    reset package-locks

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+require('dotenv').config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173,
+    strictPort: true
+}
 })
