f6b13f1e89d8f4501ab89fda2054073fa4288b4b
jOwl <jhome@js-MacBook-Pro-2.local>
2024-05-03 12:58:52 -0700

    disabled server for now

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose';
+
+mongoose.connect("mongodb+srv://justinschoi93:password1234567890@cluster0.juhayez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+
+
